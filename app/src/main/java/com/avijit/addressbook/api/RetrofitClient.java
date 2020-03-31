0299f83ff9f1a66f19bbf815d5ea5ef262766907
Avijit Bairagi <ovi821@gmail.com>
2020-04-01 04:25:23 +0600

    retrofit client and api list added

---
+++ app/src/main/java/com/avijit/addressbook/api/RetrofitClient.java
@@ -0,0 +1,40 @@
+package com.avijit.addressbook.api;
+
+import com.avijit.addressbook.common.Constants;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static RetrofitClient retrofitClient;
+    private Retrofit retrofit;
+
+    private RetrofitClient() {
+
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(Constants.READ_TIME_OUT, TimeUnit.SECONDS)
+                .connectTimeout(Constants.CONNECT_TIME_OUT, TimeUnit.SECONDS)
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+
+    public static synchronized RetrofitClient getInstance() {
+        if (retrofitClient == null)
+            retrofitClient = new RetrofitClient();
+
+        return retrofitClient;
+    }
+
+    public Api getApi() {
+        return retrofit.create(Api.class);
+    }
+}
