7193b2eb6bce95443afe008b0286673ae8e15806
Avijit Bairagi <ovi821@gmail.com>
2020-04-01 04:24:14 +0600

    constants file added

---
+++ app/src/main/java/com/avijit/addressbook/common/Constants.java
@@ -0,0 +1,9 @@
+package com.avijit.addressbook.common;
+
+public class Constants {
+
+    public static final String BASE_URL = "http://localhost:8999/addressbook/";
+
+    public static final Long READ_TIME_OUT = 60l;
+    public static final Long CONNECT_TIME_OUT = 60l;
+}
